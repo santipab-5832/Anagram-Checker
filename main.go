0ed4c0a8d6ee600a419307d95d7258927fc9c7cb
Santipab5832 <santiphap5832@gmail.com>
2026-06-11 15:42:51 +0700

    Setup App structure"

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func anagramCheck(word1 string, word2 string) bool {
+	
+	return true
+}
+
+func main() {
+	var word1 = "listen"
+	var word2 = "silent"
+	isAnagram := anagramCheck(word1, word2)
+
+	if isAnagram == true {
+		fmt.Println("It's Anagram")
+	} else {
+		fmt.Println("It's not Anagram")
+	}
+}
