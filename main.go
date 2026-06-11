e77259529ece6881dd95c8dc48cc0de7c9c924f5
Santipab5832 <santiphap5832@gmail.com>
2026-06-11 16:49:23 +0700

    Finished First logic

--- main.go
+++ main.go
@@ -3,8 +3,26 @@ package main
 import "fmt"
 
 func anagramCheck(word1 string, word2 string) bool {
-	
-	return true
+	count := 0
+	if len(word1) == len(word2) {
+		runes1 := []rune(word1)
+		runes2 := []rune(word2)
+		for _, v1 := range runes1 {
+			for _, v2 := range runes2 {
+				if v1 == v2 {
+					count++
+				}
+			}
+		}
+		checkWord := len(word1)
+		if count == checkWord {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
 }
 
 func main() {
